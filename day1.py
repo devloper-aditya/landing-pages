0f864d261d74ce64dbd94980dd044698b25b5495
devloper-aditya <adityasinghadityasingh995@gmail.com>
2024-09-09 15:01:46 +0530

    Myfirstlandingpage

---
+++ day1.py
@@ -0,0 +1,14 @@
+#sets
+
+set1={1,2,3,4,"aditya",False}
+print(set1)
+
+myset={"aditya","singh","cherry"}
+print(myset)
+
+myset={"aditya","singh","kumar",True,1,2}
+print(myset)
+
+
+myset={"apple","banana","cherry","apple","papaya"}
+print(myset)
---
+++ day2.py
@@ -0,0 +1,36 @@
+a = 3.6
+print(type(a))
+
+
+#displying a string
+str1=("mr abhay kumari")
+print(len(str1))
+print(type(str1))
+
+#displaying multiple values
+name="aditya singh"
+age= 21
+print("name:",name,"Age:",age)
+print(len(name))
+
+#printing variables and literals
+x=12
+y=12
+print("x=",x,"y=",y,"sum=",x+y)
+
+#printing with formating
+percentage=85.90
+print("Score:{:.2f}%".format(percentage))
+
+#printing if else statement
+a=12
+b=10
+if x>y:
+    print("a is a greater than b")
+    
+else:
+    print("b is less than or equal to a")
+
+
+    
+
